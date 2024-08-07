084b0e2231c1b4687bee28425847c0dea015eda0
DuaaJaved205 <duaajaved205@gmail.com>
2024-08-07 13:35:53 -0700

    divide.py

--- divide.py
+++ divide.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def div(num1,num2):
     if num2 == 0:
-        st.error('Cannot divide by zero! Please enter a non-zero value for the second number.')
+        st.error('Caught a ZeroDivisionError: division by zero! Please enter a non-zero value for the second number.')
     else:
         result = num1 / num2
         st.success(f'Result: {result}')
